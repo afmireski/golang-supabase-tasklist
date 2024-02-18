b5400dc45f4a980ede32c26f492a73577173fd91
afmireski <afmireski@outlook.com>
2024-02-18 16:09:46 -0300

    refactor: change CreatorRepository input type to entities.Creator

--- internal/ports/creator-repository-port.go
+++ internal/ports/creator-repository-port.go
@@ -2,13 +2,8 @@ package ports
 
 import "github.com/afmireski/golang-supabase-tasklist/internal/entities"
 
-type CreateCreatorInput struct {
-	Name  string
-	Email string
-}
-
 type CreatorRepository interface {
-	Create(input CreateCreatorInput) error
+	Create(input entities.Creator) error
 	FindById(id int32) (*entities.Creator, error)
 	FindByEmail(email string) (*entities.Creator, error)
 }
